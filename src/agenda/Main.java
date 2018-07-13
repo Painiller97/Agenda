1a3de610d74a37390789c3ff2311a72ad847e172
PcI5 <PcI5@PcI5-PC>
2018-07-13 17:30:54 +0200

    Ejemplo de hola

--- src/agenda/Main.java
+++ src/agenda/Main.java
@@ -49,7 +49,7 @@ public class Main {
                     for (int i = 0; i < contactos.size(); i++) {
                         System.out.println(contactos.get(i));
                     }
-                    
+                    System.out.println("Hola mundo");
                     break;
                 case 3:
                     escribir(contactos);
