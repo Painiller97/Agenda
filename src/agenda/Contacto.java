f73fabc5abdddad73b0974f6aaac59b931646fab
Painiller97 <derteufel39@gmail.com>
2018-07-13 17:06:17 +0200

    initial commit

---
+++ src/agenda/Contacto.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+import java.io.Serializable;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aul4-1
+ */
+public class Contacto implements Serializable{
+    private String nombre;
+    private int numero;
+
+    public Contacto() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" + "nombre=" + nombre + ", numero=" + numero + '}';
+    }
+    
+    public void agregar(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el nombre.");
+        this.nombre = sc.nextLine();
+        System.out.println("Escribe el número.");
+        this.numero = sc.nextInt();
+    }
+    
+}
---
+++ src/agenda/Main.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Aul4-1
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ArrayList<Contacto> contactos = new ArrayList<Contacto>();
+        ArrayList<Contacto> contactos = (ArrayList<Contacto>) leer();
+        Contacto c;
+
+        Scanner sc = new Scanner(System.in);
+        boolean salir=false;
+        while (!salir) {
+            System.out.println("¿Qué quieres hacer?");
+            System.out.println("1 - Agregar contacto");
+            System.out.println("2 - Ver contactos");
+            System.out.println("3 - Guardar y Salir");
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Agrega el contacto");
+                    c = new Contacto();
+                    c.agregar();
+                    contactos.add(c);
+                    break;
+                case 2:
+                    for (int i = 0; i < contactos.size(); i++) {
+                        System.out.println(contactos.get(i));
+                    }
+                    break;
+                case 3:
+                    escribir(contactos);
+                    salir=true;
+                    break;
+            }
+        }
+    }
+
+    public static void escribir(ArrayList contactos) {
+        try {
+            FileOutputStream fos = new FileOutputStream("data.dat");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(contactos);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static Object leer() {
+        Object o = null;
+
+        try {
+            FileInputStream fos = new FileInputStream("data.dat");
+            ObjectInputStream oos = new ObjectInputStream(fos);
+            o = oos.readObject();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return o;
+    }
+}
